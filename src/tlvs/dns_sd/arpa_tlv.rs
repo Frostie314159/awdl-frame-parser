b5efa0fe3a7b7e2319945879943641e10610ef99
Frostie314159 <iam.an.programmer@gmail.com>
2023-06-29 18:28:58 +0200

    Removed flags from ArpaTLV.

--- src/tlvs/dns_sd/arpa_tlv.rs
+++ src/tlvs/dns_sd/arpa_tlv.rs
@@ -9,24 +9,24 @@ use alloc::borrow::Cow;
 #[derive(Clone, Default, PartialEq, Eq)]
 /// A TLV containing the hostname of the peer. Used for reverse DNS.
 pub struct ArpaTLV<'a> {
-    /// A currently unknown flags header.
-    pub flags: u8,
-
     /// The actual arpa data.
     pub arpa: AWDLDnsName<'a>,
 }
 #[cfg(feature = "read")]
 impl<'a> Read for ArpaTLV<'a> {
     fn from_bytes(data: &mut impl ExactSizeIterator<Item = u8>) -> Result<Self, ParserError> {
-        let flags = data.next().ok_or(ParserError::TooLittleData(1))?;
+        let flags = data.next().ok_or(ParserError::TooLittleData(1))?; // Always 0x03.
+        if flags != 0x03 {
+            return Err(ParserError::InvalidMagic);
+        }
         let arpa = AWDLDnsName::from_bytes(data)?;
-        Ok(Self { flags, arpa })
+        Ok(Self { arpa })
     }
 }
 #[cfg(feature = "write")]
 impl<'a> Write<'a> for ArpaTLV<'a> {
     fn to_bytes(&self) -> Cow<'a, [u8]> {
-        core::iter::once(self.flags)
+        core::iter::once(0x03)
             .chain(self.arpa.to_bytes().iter().copied())
             .collect()
     }
@@ -35,7 +35,7 @@ impl_tlv_conversion!(false, ArpaTLV<'a>, TLVType::Arpa, 3);
 #[cfg(test)]
 #[test]
 fn test_arpa_tlv() {
-    use crate::common::awdl_dns_compression::AWDLDnsCompression;
+    use crate::common::AWDLDnsCompression;
     use alloc::{borrow::ToOwned, vec};
 
     let bytes = include_bytes!("../../../test_bins/arpa_tlv.bin")[3..].to_vec();
@@ -44,7 +44,6 @@ fn test_arpa_tlv() {
     assert_eq!(
         arpa_tlv,
         ArpaTLV {
-            flags: 0x03,
             arpa: AWDLDnsName {
                 labels: vec!["simon-framework".into()],
                 domain: AWDLDnsCompression::Local
